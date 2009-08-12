e38b05f590a75ca7aaef5a8f6c88062b39fc1e00
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-08-12 01:46:35 +0000

    Include full documentation for all macros.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@10 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- include/DHHookCommon.h
+++ include/DHHookCommon.h
@@ -1,15 +1,64 @@
 #import <substrate.h>
 
+/*
+ * All hook names are created/specified with $ in place of : for selector names.
+ */
+
+/*
+ * HOOK(class, name, type, args...)
+ *
+ * Example:
+ * 	HOOK(Class, init, id) {
+ * 	  ...
+ * 	}
+ *
+ * 	HOOK(Class, initWithFrame$andOtherThing$, id, CGRect frame, id otherThing) {
+ * 	  ...
+ * 	}
+ *
+ * Creates a static variable to store the original message address, and a function to replace it.
+ * type is the return type, and args are all the message arguments, in order; args is optional.
+ */
 #define HOOK(class, name, type, args...) \
 	static type (*_ ## class ## $ ## name)(class *self, SEL sel, ## args); \
 	static type $ ## class ## $ ## name(class *self, SEL sel, ## args)
 
+/*
+ * CALL_ORIG(class, name, args...)
+ *
+ * Example:
+ * 	CALL_ORIG(Class, init);
+ * 	CALL_ORIG(Class, initWithFrame$andOtherThing, frame, otherThing);
+ *
+ * Calls an original implementation.
+ */
 #define CALL_ORIG(class, name, args...) \
 	_ ## class ## $ ## name(self, sel, ## args)
 
+/*
+ * GET_CLASS(class)
+ *
+ * Example:
+ * 	GET_CLASS(UIToolbarButton);
+ *
+ * Simply creates a variable (named $class) to store a class for the HOOK_MESSAGE_* macros.
+ * To avoid having to call objc_getClass over and over, and to provide a uniform naming scheme.
+ */
 #define GET_CLASS(class) \
 	Class $ ## class = objc_getClass(#class)
 
+/*
+ * HOOK_MESSAGE(class, sel)
+ *
+ * Example:
+ * 	HOOK_MESSAGE(Class, init);
+ *
+ * Saves the original implementation of sel to a static variable named after _class$sel (created with HOOK), after
+ * replacing it with $class$sel (created with HOOK).
+ *
+ * This exists because sometimes you just want to hook a message with no args, without having to specify a replacement
+ * or call __getsel
+ */
 #define HOOK_MESSAGE(class, sel) \
 	_ ## class ## $ ## sel = MSHookMessage($ ## class, @selector(sel), &$ ## class ## $ ## sel) 
 
@@ -20,9 +69,30 @@ static inline SEL __getsel(const char *in) {
 		selector[i] = (in[i] == '$' ? ':' : in[i]);
 	return sel_getUid(selector);
 }
-#define HOOK_MESSAGE_AUTO(class, sel) \
-	_ ## class ## $ ## sel = MSHookMessage($ ## class, __getsel(#sel), &$ ## class ## $ ## sel)
+/*
+ * HOOK_MESSAGE_AUTO(class, replace)
+ *
+ * Example:
+ * 	HOOK_MESSAGE_AUTO(Class, initWithFrame$andOtherThing$andAnotherThing$);
+ *
+ * Beware, __getsel (string copy/transform) is called every time this macro is used.
+ * Automatically turns a replacement selector in the $ format into a SEL, as a shorter form of HOOK_MESSAGE_REPLACEMENT.
+ *
+ * Saves the original implementation to a static variable named after _class$replace (created with HOOK), after
+ * replacing it with $class$replace (created with HOOK).
+ */
+#define HOOK_MESSAGE_AUTO(class, replace) \
+	_ ## class ## $ ## replace = MSHookMessage($ ## class, __getsel(#replace), &$ ## class ## $ ## replace)
 
+/*
+ * HOOK_MESSAGE_REPLACEMENT(class, sel, replace)
+ *
+ * Example:
+ * 	HOOK_MESSAGE_REPLACEMENT(Class, initWithFrame:andOtherThing:andAnotherThing:, initWithFrame$andOtherThing$andAnotherThing$);
+ *
+ * Saves the original implementation to a static variable named after _class$replace (created with HOOK), after
+ * replacing it with $class$replace (created with HOOK).
+ */
 #define HOOK_MESSAGE_REPLACEMENT(class, sel, replace) \
 	_ ## class ## $ ## replace = MSHookMessage($ ## class, @selector(sel), &$ ## class ## $ ## replace)
 
