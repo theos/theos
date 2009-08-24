c8b89cbdcb3816d304a6b501d6d9469c00ee74da
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-08-24 02:06:47 +0000

    Include DHCommon, which is based on parts of rpetrich's Captain Hook. Update the makefile to remove init (from the ld call) and use constructors instead.
    git-svn-id: http://svn.howett.net/svn/iphone-framework@21 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

---
+++ include/DHCommon.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+/* Common, useful things.
+ * A bit has been lifted from rpetrich's Captain Hook macros. Thanks, Ryan!
+ */
+#define _Constructor __attribute__((constructor))
+#define DHDeclareClass(name) static Class $ ## name
+#define DHLoadLateClass(name) $ ## name = objc_getClass(#name)
+#define DHLoadEarlyClass(name) $ ## name = [name class]
+#define DHClass(name) $ ## name
+
+
+static inline void _DHRelease(id object) __attribute__((always_inline));
+static inline void _DHRelease(id object) {
+	[object release];
+}
+#define DHScopeReleased __attribute__((cleanup(_DHRelease)))
+#define DHScopedAutoreleasePool() NSAutoreleasePool *DHScopedAutoreleasePool __attribute__((cleanup(_DHRelease),unused)) = [[NSAutoreleasePool alloc] init]
+
+// vim:ft=objc
