1fa7961b9d0e5722293257a5c812eb445adfbcc2
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-10-28 00:17:54 +0000

    Add a fakeroot wrapper that supports required/optional commands and fakeroot-ng, fakeroot, and no-op.
    git-svn-id: http://svn.howett.net/svn/iphone-framework@29 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

---
+++ scripts/fakeroot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+required=0
+persistence=/tmp/dhbxxx
+
+while getopts ":p:r" flag; do
+	case "$flag" in
+		:)	echo "$0: Option -$OPTARG requires an argument." 1>&2
+			exit 1
+			;;
+		\?)	echo "$0: Option -$OPTARG unrecognized." 1>&2
+			exit 1
+			;;
+		p)	persistence="$OPTARG" ;;
+		r)	required=1 ;;
+	esac
+done
+shift $((OPTIND-1))
+cmd=$*
+
+rm -f $persistence
+
+if test fakeroot-ng &> /dev/null; then
+	fakeroot="fakeroot-ng -p $persistence -- "
+elif test fakeroot &> /dev/null; then
+	fakeroot="fakeroot -s $persistence -- "
+else
+	if [[ $required -eq 1 ]]; then
+		fakeroot=""
+	else
+		fakeroot=": "
+	fi
+fi
+
+#echo $fakeroot $cmd
+$fakeroot $cmd
