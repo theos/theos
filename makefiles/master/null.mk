8974689709d0783a23eb6b224bf481f7c42dace2
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-12-17 19:42:14 +0000

    Add a null project type (good for packaging without compiling anything).
    git-svn-id: http://svn.howett.net/svn/iphone-framework@74 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

---
+++ makefiles/master/null.mk
@@ -0,0 +1,17 @@
+NULL_NAME := $(strip $(NULL_NAME))
+
+ifeq ($(FW_RULES_LOADED),)
+include $(FW_MAKEDIR)/rules.mk
+endif
+
+internal-all::
+
+internal-package:: $(NULL_NAME:=.package.null.variables);
+
+NULLS_WITH_SUBPROJECTS = $(strip $(foreach null,$(NULL_NAME),$(patsubst %,$(null),$($(null)_SUBPROJECTS))))
+ifneq ($(NULLS_WITH_SUBPROJECTS),)
+internal-clean:: $(NULLS_WITH_SUBPROJECTS:=.clean.null.subprojects)
+endif
+
+$(NULL_NAME):
+	@$(MAKE) -f $(MAKEFILE_NAME) --no-print-directory --no-keep-going $@.all.null.variables
