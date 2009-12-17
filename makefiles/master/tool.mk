4c5a247441810ec34ebec402f3bde1888a976dd0
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-12-17 00:23:07 +0000

    Add a tool project (similar to GNUstep's tool) - simple commandline tool that doesn't link UIKit.
    git-svn-id: http://svn.howett.net/svn/iphone-framework@55 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

---
+++ makefiles/master/tool.mk
@@ -0,0 +1,17 @@
+TOOL_NAME := $(strip $(TOOL_NAME))
+
+ifeq ($(FW_RULES_LOADED),)
+include $(FW_MAKEDIR)/rules.mk
+endif
+
+internal-all:: $(TOOL_NAME:=.all.tool.variables);
+
+internal-stage:: $(TOOL_NAME:=.stage.tool.variables);
+
+TOOLS_WITH_SUBPROJECTS = $(strip $(foreach tool,$(TOOL_NAME),$(patsubst %,$(tool),$($(tool)_SUBPROJECTS))))
+ifneq ($(TOOLS_WITH_SUBPROJECTS),)
+internal-clean:: $(TOOLS_WITH_SUBPROJECTS:=.clean.tool.subprojects)
+endif
+
+$(TOOL_NAME):
+	@$(MAKE) -f $(MAKEFILE_NAME) --no-print-directory --no-keep-going $@.all.tool.variables
