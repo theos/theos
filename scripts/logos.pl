1038708e1c4187556aa5d2b71184bc356c5314c6
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-12-20 12:51:16 +0000

    Comments and a couple small logic changes: class names are [$\w]+ and use ! instead of  eq "".
    git-svn-id: http://svn.howett.net/svn/iphone-framework@80 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- scripts/logos.pl
+++ scripts/logos.pl
@@ -16,6 +16,7 @@ $numselectors = 0;
 @argreturns = ();
 $argcount = 0;
 while($line = <FILE>) {
+	# Search for a discrete %x% or an open-ended %x
 	if($line =~ /(%(.*?)($|%))/) {
 		my $cmdwrapper = $1;
 		my $cmdspec = $2;
@@ -39,16 +40,19 @@ sub parseCommand {
 
 	$replacement = "";
 	if($command eq "hook") {
-		$cmdspec =~ /^\s*(.+?)\s+/;
+		# Hook Macro Syntax
+		# %hook Class [+|-](returnvalue)keyword:(argtype)arg keyword:(argtype)arg
+		# %hook Class [+|-](returnvalue)keyword
+		$cmdspec =~ /^\s*([\$\w]+?)\s+/; # Any identifier, bounded by any number of spaces
 		$cmdspec = $';
 		$class = $1;
 
-		$cmdspec =~ /^\s*([-+]?)\s*/;
+		$cmdspec =~ /^\s*([-+]?)\s*/; # + or - bounded by any number of spaces
 		$cmdspec = $';
 		my $scope = "instance";
 		$scope = "class" if $1 && $1 eq "+";
 
-		$cmdspec =~ /^\s*\(\s*(.+?)\s*\)\s*/;
+		$cmdspec =~ /^\s*\(\s*(.+?)\s*\)\s*/; # (returntype) with any number of spaces
 		$cmdspec = $';
 		$return = $1;
 
@@ -60,15 +64,17 @@ sub parseCommand {
 		# Yeah it's a hack to avoid finding a simple selector after a complex one.
 		my $complexselector = 0;
 
+		# word, then an optional: ": (argtype)argname"
 		while($cmdspec =~ /([\$\w]+)(:[\s]*\((.+?)\)[\s]*([\$\w]+?)($|\s+))?/) {
 			$keyword = $1;
 			if(!$2 && $complexselector != 1) {
 				$selector = $keyword;
 				$cmdspec = $';
 				last;
-			} elsif ($2 eq "" && $complexselector == 1) {
+			} elsif (!$2 && $complexselector == 1) {
 				last;
 			} else {
+				# build our selectors out of of keywords concat'd with :s
 				$selector .= $keyword.":";
 				$argreturn = $3;
 				$argname = $4;
@@ -100,6 +106,10 @@ sub parseCommand {
 		$replacement = "CALL_ORIG($class, $newselector";
 		my $hasparens = 0;
 		if($cmdspec) {
+			# Walk a string char-by-char, noting parenthesis depth.
+			# If we encounter a ) that puts us back at zero, we found a (
+			# and have reached its closing ).
+			# This does not account for things like (")") though. Meh.
 			my $parenmatch = $cmdspec;
 			my $pdepth = 0;
 			my $endindex = 0;
