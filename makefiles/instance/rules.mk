328c3d766e4539bf78d875427f996e2d161ba271
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2009-12-22 20:18:13 +0000

    Add it to the objcflags, not the cflags... Whoops.
    git-svn-id: http://svn.howett.net/svn/iphone-framework@123 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/instance/rules.mk
+++ makefiles/instance/rules.mk
@@ -26,7 +26,8 @@ ifneq ($(OBJC_OBJS)$(OBJCC_OBJS),)
 
 	# Add all private frameworks from the type and instance, as well as -F for the private framework dir.
 	ifneq ($($(FW_TYPE)_PRIVATE_FRAMEWORKS)$($(FW_INSTANCE)_PRIVATE_FRAMEWORKS),)
-		ADDITIONAL_CFLAGS += -F/System/Library/PrivateFrameworks
+		ADDITIONAL_OBJCFLAGS += -F/System/Library/PrivateFrameworks
+		ADDITIONAL_OBJCCFLAGS += -F/System/Library/PrivateFrameworks
 		ADDITIONAL_LDFLAGS += -F/System/Library/PrivateFrameworks
 	endif
 
