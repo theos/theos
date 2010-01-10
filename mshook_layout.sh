ee7b142f4c8e37312afc2a144dbe5bb8b56d1a87
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-01-10 00:58:21 +0000

    Include some more interesting boilerplate stuff in mshook_layout.
    git-svn-id: http://svn.howett.net/svn/iphone-framework@164 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- mshook_layout.sh
+++ mshook_layout.sh
@@ -28,5 +28,39 @@ include framework/makefiles/tweak.mk
 __END
 
 cat > Tweak.xm << __END
-// %hook Blah -(void)blah { ... }
+/* How to Hook with Logos
+Hooks are written with syntax similar to that of an Objective-C @implementation.
+You don't need to #include <substrate.h>, it will be done automatically, as will
+the generation of a class list and an automatic constructor.
+
+%hook ClassName
+
+// Hooking a class method
++ (id)sharedInstance {
+	return %orig;
+}
+
+// Hooking an instance method with an argument.
+- (void)messageName:(int)argument {
+	%log; // Write a message about this call, including its class, name and arguments, to the system log.
+
+	%orig; // Call through to the original function with its original arguments.
+	%orig(nil); // Call through to the original function with a custom argument.
+
+	// If you use %orig(), you MUST supply all arguments (except for self and _cmd, the automatically generated ones.)
+}
+
+// Hooking an instance method with no arguments.
+- (id)noArguments {
+	%log;
+	id awesome = %orig;
+	[awesome doSomethingElse];
+
+	return awesome;
+}
+
+// Always make sure you clean up after yourself; Not doing so could have grave conseqeuences!
+%end
+*/
+
 __END
