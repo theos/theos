88f3f7b780fe264282b42fa054a250aa3923ad87
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-01-12 03:36:15 +0000

    Use object_getClass(class) instead of objc_getMetaClass("class") for the metaclass of a subclass.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@180 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- scripts/lib/Logos/Subclass.pm
+++ scripts/lib/Logos/Subclass.pm
@@ -53,7 +53,7 @@ sub initializers {
 		$return .= "Class \$\$$_ = \$$_; ";
 	}
 	foreach(keys %{$self->{USEDMETACLASSES}}) {
-		$return .= "Class \$\$meta\$$_ = objc_getMetaClass(\"$_\"); ";
+		$return .= "Class \$\$meta\$$_ = object_getClass(\$\$$_); ";
 	}
 	foreach(@{$self->{METHODS}}) {
 		$return .= $_->buildHookCall;
