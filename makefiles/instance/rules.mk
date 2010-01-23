ea939ec2ba548003d9ba13d0f0fcf707c6fb4dd9
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-01-23 05:27:51 +0000

    Allow the inclusion of environment-level FLAGS variables in ALL_FLAGS, and use ADDITIONAL_OBJCCFLAGS and ADDITIONAL_CCFLAGS in their respective ALL_FLAGS variables. Only use ADDITIONAL_FLAGS for flags specified directly by the project or the user (only!). Use AUXILIARY_FLAGS for all flags added or generated by a Makefile (and fix all instance makefiles to not += ALL_FLAGS, but to add that stuff to AUXILIARY_FLAGS.)
    git-svn-id: http://svn.howett.net/svn/iphone-framework@191 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/instance/rules.mk
+++ makefiles/instance/rules.mk
@@ -9,35 +9,34 @@ C_OBJS = $(patsubst %.c,%.c.o,$($(FW_INSTANCE)_C_FILES))
 OBJ_FILES = $(strip $(OBJCC_OBJS) $(OBJC_OBJS) $(CC_OBJS) $(C_OBJS))
 OBJ_FILES_TO_LINK = $(addprefix $(FW_OBJ_DIR)/,$(OBJ_FILES)) $($(FW_INSTANCE)_OBJ_FILES)
 
-ADDITIONAL_CPPFLAGS += $($(FW_TYPE)_CPPFLAGS) $($(FW_INSTANCE)_CPPFLAGS)
-ADDITIONAL_CFLAGS += $($(FW_TYPE)_CFLAGS) $($(FW_INSTANCE)_CFLAGS)
-ADDITIONAL_OBJCFLAGS += $($(FW_TYPE)_OBJCFLAGS) $($(FW_INSTANCE)_OBJCFLAGS)
-ADDITIONAL_CCFLAGS += $($(FW_TYPE)_CCFLAGS) $($(FW_INSTANCE)_CCFLAGS)
-ADDITIONAL_OBJCCFLAGS += $($(FW_TYPE)_OBJCCFLAGS) $($(FW_INSTANCE)_OBJCCFLAGS)
-ADDITIONAL_LDFLAGS += $($(FW_TYPE)_LDFLAGS) $($(FW_INSTANCE)_LDFLAGS)
+ADDITIONAL_CPPFLAGS += $($(FW_INSTANCE)_CPPFLAGS)
+ADDITIONAL_CFLAGS += $($(FW_INSTANCE)_CFLAGS)
+ADDITIONAL_OBJCFLAGS += $($(FW_INSTANCE)_OBJCFLAGS)
+ADDITIONAL_CCFLAGS += $($(FW_INSTANCE)_CCFLAGS)
+ADDITIONAL_OBJCCFLAGS += $($(FW_INSTANCE)_OBJCCFLAGS)
+ADDITIONAL_LDFLAGS += $($(FW_INSTANCE)_LDFLAGS)
 
 # If we have any Objective-C objects, link Foundation and libobjc.
 ifneq ($(OBJC_OBJS)$(OBJCC_OBJS),)
-	ADDITIONAL_LDFLAGS += -lobjc -framework Foundation -framework CoreFoundation
+	AUXILIARY_LDFLAGS += -lobjc -framework Foundation -framework CoreFoundation
 
 	# Add all frameworks from the type and instance.
-	ADDITIONAL_LDFLAGS += $(foreach framework,$($(FW_TYPE)_FRAMEWORKS),-framework $(framework))
-	ADDITIONAL_LDFLAGS += $(foreach framework,$($(FW_INSTANCE)_FRAMEWORKS),-framework $(framework))
+	AUXILIARY_LDFLAGS += $(foreach framework,$($(FW_TYPE)_FRAMEWORKS),-framework $(framework))
+	AUXILIARY_LDFLAGS += $(foreach framework,$($(FW_INSTANCE)_FRAMEWORKS),-framework $(framework))
 
 	# Add all private frameworks from the type and instance, as well as -F for the private framework dir.
 	ifneq ($($(FW_TYPE)_PRIVATE_FRAMEWORKS)$($(FW_INSTANCE)_PRIVATE_FRAMEWORKS),)
-		ADDITIONAL_OBJCFLAGS += -F/System/Library/PrivateFrameworks
-		ADDITIONAL_OBJCCFLAGS += -F/System/Library/PrivateFrameworks
-		ADDITIONAL_LDFLAGS += -F/System/Library/PrivateFrameworks
+		AUXILIARY_OBJCFLAGS += -F/System/Library/PrivateFrameworks
+		AUXILIARY_LDFLAGS += -F/System/Library/PrivateFrameworks
 	endif
 
-	ADDITIONAL_LDFLAGS += $(foreach framework,$($(FW_TYPE)_PRIVATE_FRAMEWORKS),-framework $(framework))
-	ADDITIONAL_LDFLAGS += $(foreach framework,$($(FW_INSTANCE)_PRIVATE_FRAMEWORKS),-framework $(framework))
+	AUXILIARY_LDFLAGS += $(foreach framework,$($(FW_TYPE)_PRIVATE_FRAMEWORKS),-framework $(framework))
+	AUXILIARY_LDFLAGS += $(foreach framework,$($(FW_INSTANCE)_PRIVATE_FRAMEWORKS),-framework $(framework))
 endif
 
 # In addition, if we have any Objective-C++, add the ObjC++ linker flags.
 ifneq ($(OBJCC_OBJS),)
-	ADDITIONAL_LDFLAGS += -ObjC++ -fobjc-exceptions -fobjc-call-cxx-cdtors
+	AUXILIARY_LDFLAGS += -ObjC++ -fobjc-exceptions -fobjc-call-cxx-cdtors
 endif
 
 before-$(FW_INSTANCE)-all::
