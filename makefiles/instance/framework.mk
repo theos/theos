40362984b7b66179b05e270d96566e361d7b7496
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-02-04 09:16:36 +0000

    Add proper support for parallel making. Parallel building is turned off until we're in the last stage of building an instance: all subprojects of an aggregate are built sequentially, and all targets in an instance are built in parallel. This is achieved by having an internal-x-all_ rule that spawns a submake of internal-x-compile with _FW_MAKE_PARALLEL set to yes, which causes rules.mk to omit the .NOTPARALLEL target.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@205 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/instance/framework.mk
+++ makefiles/instance/framework.mk
@@ -2,7 +2,7 @@ ifeq ($(FW_RULES_LOADED),)
 include $(FW_MAKEDIR)/rules.mk
 endif
 
-.PHONY: internal-framework-all_ internal-framework-package_
+.PHONY: internal-framework-all_ internal-framework-package_ internal-framework-compile
 
 ifeq ($($(FW_INSTANCE)_FRAMEWORK_NAME),)
 LOCAL_FRAMEWORK_NAME = $(FW_INSTANCE)
@@ -12,7 +12,17 @@ endif
 
 AUXILIARY_LDFLAGS += -dynamiclib -install_name $($(FW_INSTANCE)_INSTALL_PATH)/$(LOCAL_FRAMEWORK_NAME).framework/$(FW_INSTANCE)
 
+ifeq ($(FW_MAKE_PARALLEL_BUILDING), no)
 internal-framework-all_:: $(FW_OBJ_DIR) $(FW_OBJ_DIR)/$(FW_INSTANCE)
+else
+internal-framework-all_:: $(FW_OBJ_DIR)
+	$(ECHO_NOTHING)$(MAKE) --no-print-directory --no-keep-going \
+		internal-framework-compile \
+		FW_TYPE=$(FW_TYPE) FW_INSTANCE=$(FW_INSTANCE) FW_OPERATION=compile \
+		FW_BUILD_DIR="$(FW_BUILD_DIR)" _FW_MAKE_PARALLEL=yes$(ECHO_END)
+
+internal-framework-compile: $(FW_OBJ_DIR)/$(FW_INSTANCE)
+endif
 
 $(FW_OBJ_DIR)/$(FW_INSTANCE): $(OBJ_FILES_TO_LINK)
 	$(ECHO_LINKING)$(CXX) $(ALL_LDFLAGS) -o $@ $^$(ECHO_END)
