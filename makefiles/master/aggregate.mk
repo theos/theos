ca04b04ecaad14d39ad29af6006f0256d6d4e260
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-02-27 09:13:03 +0000

    In internal-install in an aggregate, run internal-install for all the SUBPROJECTS, so that, for example, an aggregate with a tweak subproject will respring.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@216 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/master/aggregate.mk
+++ makefiles/master/aggregate.mk
@@ -21,4 +21,22 @@ internal-all internal-package internal-clean::
 	  else exit $$?; \
 	  fi; \
 	done;
+
+internal-install::
+	@operation=$@; \
+	abs_build_dir=$(ABS_FW_BUILD_DIR); \
+	for d in $(SUBPROJECTS); do \
+	  echo "Running post-install rules for $$d..."; \
+	  if [ "$${abs_build_dir}" = "." ]; then \
+	    lbuilddir="."; \
+	  else \
+	    lbuilddir="$${abs_build_dir}/$$d"; \
+	  fi; \
+	  if $(MAKE) -C $$d $(FW_NO_PRINT_DIRECTORY_FLAG) --no-keep-going $$operation \
+		FW_BUILD_DIR="$$lbuilddir" \
+	     ; then\
+	     :; \
+	  else exit $$?; \
+	  fi; \
+	done;
 endif
