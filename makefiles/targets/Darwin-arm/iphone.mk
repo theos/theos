c2bbd9524de4f065d7a65ec7bdbbe0fe6f36e37a
Ryan Petrich <rpetrich@gmail.com>
2010-03-21 07:18:31 -0600

    On-device builds should now install locally

--- makefiles/targets/Darwin-arm/iphone.mk
+++ makefiles/targets/Darwin-arm/iphone.mk
@@ -1,1 +1,20 @@
-native.mk
+ifeq ($(FW_TARGET_LOADED),)
+FW_TARGET_LOADED := 1
+FW_TARGET_NAME := iphone
+
+SDKBINPATH ?= /Developer/Platforms/iPhoneOS.platform/Developer/usr/bin
+SDKVERSION ?= 3.0
+SYSROOT ?= /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$(SDKVERSION).sdk
+
+TARGET_CC ?= $(SDKBINPATH)/gcc-4.2
+TARGET_CXX ?= $(SDKBINPATH)/g++-4.2
+TARGET_STRIP ?= $(SDKBINPATH)/strip
+TARGET_CODESIGN_ALLOCATE ?= $(SDKBINPATH)/codesign_allocate
+TARGET_CODESIGN ?= ldid
+TARGET_CODESIGN_FLAGS ?= -S
+
+ARCHS ?= armv6
+SDKFLAGS := -isysroot $(SYSROOT) $(foreach ARCH,$(ARCHS),-arch $(ARCH)) -D__IPHONE_OS_VERSION_MIN_REQUIRED=__IPHONE_$(subst .,_,$(SDKVERSION))
+TARGET_CFLAGS := $(SDKFLAGS)
+TARGET_LDFLAGS := $(SDKFLAGS) -multiply_defined suppress
+endif
