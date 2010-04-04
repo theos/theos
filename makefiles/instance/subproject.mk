e5728aded3cf9b45d8e6e5b1ef0bdb7527bfad28
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-04-04 09:27:34 +0000

    Add subproject support. I had a patch for this lying around for the longest time, so I finally cleaned it up and committed it. A subproject is a set of code that is linked into one object file, which is then linked into the master project.
    git-svn-id: http://svn.howett.net/svn/iphone-framework@233 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

---
+++ makefiles/instance/subproject.mk
@@ -0,0 +1,20 @@
+ifeq ($(FW_RULES_LOADED),)
+include $(FW_MAKEDIR)/rules.mk
+endif
+
+.PHONY: internal-subproject-all_ internal-subproject-package_ internal-subproject-compile
+
+ifeq ($(FW_MAKE_PARALLEL_BUILDING), no)
+internal-subproject-all_:: $(FW_OBJ_DIR) $(FW_OBJ_DIR)/$(FW_SUBPROJECT_PRODUCT)
+else
+internal-subproject-all_:: $(FW_OBJ_DIR)
+	$(ECHO_NOTHING)$(MAKE) --no-print-directory --no-keep-going \
+		internal-subproject-compile \
+		FW_TYPE=$(FW_TYPE) FW_INSTANCE=$(FW_INSTANCE) FW_OPERATION=compile \
+		FW_BUILD_DIR="$(FW_BUILD_DIR)" _FW_MAKE_PARALLEL=yes$(ECHO_END)
+
+internal-subproject-compile: $(FW_OBJ_DIR)/$(FW_SUBPROJECT_PRODUCT)
+endif
+
+$(FW_OBJ_DIR)/$(FW_SUBPROJECT_PRODUCT): $(OBJ_FILES_TO_LINK)
+	$(ECHO_LINKING)$(TARGET_CXX) -nostdlib -r -d $(ADDITIONAL_LDFLAGS) $(TARGET_LDFLAGS) $(LDFLAGS) -o $@ $^$(ECHO_END)
---
+++ makefiles/master/subproject.mk
@@ -0,0 +1,22 @@
+SUBPROJECT_NAME := $(strip $(SUBPROJECT_NAME))
+ifneq ($(words $(SUBPROJECT_NAME)), 1)
+SUBPROJECT_NAME := $(word 1, $(SUBPROJECT_NAME))
+$(warning Only a single subproject can be built in any directory!)
+$(warning Ignoring all subprojects and building only $(SUBPROJECT_NAME))
+endif
+
+ifeq ($(FW_RULES_LOADED),)
+include $(FW_MAKEDIR)/rules.mk
+endif
+
+internal-all:: $(SUBPROJECT_NAME:=.all.subproject.variables);
+
+internal-package:: $(SUBPROJECT_NAME:=.package.subproject.variables);
+
+SUBPROJECTS_WITH_SUBPROJECTS = $(strip $(foreach subproject,$(SUBPROJECT_NAME),$(patsubst %,$(subproject),$($(subproject)_SUBPROJECTS))))
+ifneq ($(SUBPROJECTS_WITH_SUBPROJECTS),)
+internal-clean:: $(SUBPROJECTS_WITH_SUBPROJECTS:=.clean.subproject.subprojects)
+endif
+
+$(SUBPROJECT_NAME):
+	@$(MAKE) -f $(MAKEFILE_NAME) --no-print-directory --no-keep-going $@.all.subproject.variables
