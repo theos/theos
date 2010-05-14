d22a01f50596126e154e7e76524385ff90354182
Ryan Petrich <rpetrich@gmail.com>
2010-05-13 23:37:33 -0600

    Split FW_PACKAGE_BUILDNUM into a longer if statement

--- makefiles/package.mk
+++ makefiles/package.mk
@@ -66,7 +66,11 @@ package-build-deb-buildno::
 ifeq ($(FW_HAS_LAYOUT),1) # If we have a layout/ directory, copy layout/DEBIAN to the staging directory.
 	$(ECHO_NOTHING)rsync -a "$(FW_PROJECT_DIR)/layout/DEBIAN/" "$(FW_STAGING_DIR)/DEBIAN" $(FW_RSYNC_EXCLUDES)$(ECHO_END)
 endif # FW_HAS_LAYOUT
-	$(ECHO_NOTHING)sed -e 's/Version: \(.*\)/Version: \1$(if $(PACKAGE_BUILDNUM),-$(FW_PACKAGE_BUILDNUM),)$(if $(PACKAGE_BUILDNAME),+$(PACKAGE_BUILDNAME),)/g' "$(FW_PACKAGE_CONTROL_PATH)" > "$(FW_STAGING_DIR)/DEBIAN/control"$(ECHO_END)
+ifeq ($(FW_PACKAGE_BUILDNUM),)
+	$(ECHO_NOTHING)sed -e 's/Version: \(.*\)/Version: \1$(if $(PACKAGE_BUILDNAME),+$(PACKAGE_BUILDNAME),)/g' "$(FW_PACKAGE_CONTROL_PATH)" > "$(FW_STAGING_DIR)/DEBIAN/control"$(ECHO_END)
+else
+	$(ECHO_NOTHING)sed -e 's/Version: \(.*\)/Version: \1-$(FW_PACKAGE_BUILDNUM)$(if $(PACKAGE_BUILDNAME),+$(PACKAGE_BUILDNAME),)/g' "$(FW_PACKAGE_CONTROL_PATH)" > "$(FW_STAGING_DIR)/DEBIAN/control"$(ECHO_END)
+endif
 	$(ECHO_NOTHING)echo "Installed-Size: $(shell du $(DU_EXCLUDE) DEBIAN -ks "$(FW_STAGING_DIR)" | cut -f 1)" >> "$(FW_STAGING_DIR)/DEBIAN/control"$(ECHO_END)
 
 package-build-deb:: package-build-deb-buildno
