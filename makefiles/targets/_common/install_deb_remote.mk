5b86265a3de5c45fe5075adbfcac7036f3e45c43
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-06-27 09:10:00 +0000

    Add install rules for the simulator. Rename the simulator target to iphone_simulator from iphonesimulator (so it becomes TARGET_IPHONE_SIMULATOR to the C preprocessor.) Fix missing punctuation in the remote install error message :P.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@285 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/targets/_common/install_deb_remote.mk
+++ makefiles/targets/_common/install_deb_remote.mk
@@ -2,7 +2,7 @@ export TARGET_REMOTE := 1
 
 ifeq ($(FW_DEVICE_IP),)
 internal-install::
-	$(info $(MAKE) install requires that you set FW_DEVICE_IP in your environment. It is also recommended that you have public-key authentication set up for root over SSH, or you'll be entering your password a lot)
+	$(info $(MAKE) install requires that you set FW_DEVICE_IP in your environment. It is also recommended that you have public-key authentication set up for root over SSH, or you'll be entering your password a lot.)
 	@exit 1
 else # FW_DEVICE_IP
 ifeq ($(FW_CAN_PACKAGE),1)
