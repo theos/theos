7f27e56584074cff7805896eb612358951d73ac6
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-06-27 09:04:04 +0000

    Allow for remote installation of projects that do not support packaging, using install.mergeDir (which has been updated to preserve permissions and allow for remote use.) Also, fix a comment in install_deb_local that was bothering me. :P
    git-svn-id: http://svn.howett.net/svn/theos/trunk@284 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/targets/_common/install_deb_local.mk
+++ makefiles/targets/_common/install_deb_local.mk
@@ -3,7 +3,7 @@ export TARGET_REMOTE := 0
 ifeq ($(FW_CAN_PACKAGE),1)
 internal-install::
 	install.exec dpkg -i $(FW_PACKAGE_FILENAME).deb
-else # FW_DEVICE_IP
+else # FW_CAN_PACKAGE
 internal-install:: stage
 	install.mergeDir "$(FW_STAGING_DIR)" "/"
 endif
--- makefiles/targets/_common/install_deb_remote.mk
+++ makefiles/targets/_common/install_deb_remote.mk
@@ -5,7 +5,12 @@ internal-install::
 	$(info $(MAKE) install requires that you set FW_DEVICE_IP in your environment. It is also recommended that you have public-key authentication set up for root over SSH, or you'll be entering your password a lot)
 	@exit 1
 else # FW_DEVICE_IP
+ifeq ($(FW_CAN_PACKAGE),1)
 internal-install::
 	install.copyFile "$(FW_PROJECT_DIR)/$(FW_PACKAGE_FILENAME).deb" "$(FW_PACKAGE_FILENAME).deb"
 	install.exec dpkg -i $(FW_PACKAGE_FILENAME).deb
-endif
+else # FW_CAN_PACKAGE == 0
+internal-install:: stage
+	install.mergeDir "$(FW_STAGING_DIR)" "/"
+endif # FW_CAN_PACKAGE
+endif # FW_DEVICE_IP
