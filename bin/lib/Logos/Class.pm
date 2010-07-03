9363958e14aff860f72e42530edfa8a3b1a250b3
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-07-03 07:03:16 +0000

    Generate a local non-metaclass variable if we use the metaclass. Otherwise, the default object_getClass($ClassName) metaclass init expression will not work.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@298 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- bin/lib/Logos/Class.pm
+++ bin/lib/Logos/Class.pm
@@ -80,7 +80,7 @@ sub addMethod {
 sub initializers {
 	my $self = shift;
 	my $return = "";
-	$return .= "Class \$\$".$self->{NAME}." = ".$self->expression."; " if $self->{INST};
+	$return .= "Class \$\$".$self->{NAME}." = ".$self->expression."; " if $self->{INST} or $self->{META};
 	$return .= "Class \$\$meta\$".$self->{NAME}." = ".$self->metaexpression."; " if $self->{META};
 	foreach(@{$self->{METHODS}}) {
 		$return .= $_->buildHookCall;
