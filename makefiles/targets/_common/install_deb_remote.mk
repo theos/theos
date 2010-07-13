fc70104215ed7c79090f5058b80cf116ec346217
dhowett <dhowett@4410221e-0ddf-4ce3-99c0-2db6c0dbc727>
2010-07-13 11:37:25 +0000

    Add support for FW_DEVICE_PORT (default 22), and fix the "FW_DEVICE_IP is not exported (when not set/exported in the calling shell)" bug.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@300 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/targets/_common/install_deb_remote.mk
+++ makefiles/targets/_common/install_deb_remote.mk
@@ -5,6 +5,11 @@ internal-install::
 	$(info $(MAKE) install requires that you set FW_DEVICE_IP in your environment. It is also recommended that you have public-key authentication set up for root over SSH, or you'll be entering your password a lot.)
 	@exit 1
 else # FW_DEVICE_IP
+
+FW_DEVICE_PORT ?= 22
+
+export FW_DEVICE_IP FW_DEVICE_PORT
+
 ifeq ($(FW_CAN_PACKAGE),1)
 internal-install::
 	install.copyFile "$(FW_PROJECT_DIR)/$(FW_PACKAGE_FILENAME).deb" "$(FW_PACKAGE_FILENAME).deb"
@@ -13,4 +18,5 @@ else # FW_CAN_PACKAGE == 0
 internal-install:: stage
 	install.mergeDir "$(FW_STAGING_DIR)" "/"
 endif # FW_CAN_PACKAGE
+
 endif # FW_DEVICE_IP
