4868405111dc0a0a2fc4c664e4f24c99f43459d7
Dustin L. Howett <dustin@howett.net>
2011-06-20 02:31:44 +0000

    [Darwin/iphone] Set the default SDKVERSION to 'latest'! Allow deployment version to be 'latest', but default to 3.0 if SDKVERSION is unchanged, so that we retain backwards compatibility. Use different internal variables so SDKVERSION changing after common.mk doesn't break everything.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@418 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/targets/Darwin/iphone.mk
+++ makefiles/targets/Darwin/iphone.mk
@@ -5,17 +5,23 @@ THEOS_TARGET_NAME := iphone
 SDKBINPATH ?= $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneOS.platform/Developer/usr/bin
 
 # A version specified as a target argument overrides all previous definitions.
-SDKVERSION := $(or $(firstword $(_THEOS_TARGET_ARGS)),$(SDKVERSION),3.0)
+_SDKVERSION := $(or $(firstword $(_THEOS_TARGET_ARGS)),$(SDKVERSION))
+_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION := $(or $(word 2,$(_THEOS_TARGET_ARGS)),$(TARGET_IPHONEOS_DEPLOYMENT_VERSION),$(_SDKVERSION),3.0)
+_THEOS_TARGET_SDK_VERSION := $(or $(_SDKVERSION),latest)
 
-ifeq ($(SDKVERSION),latest)
 _SDK_DIR := $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs
 _IOS_SDKS := $(sort $(patsubst $(_SDK_DIR)/iPhoneOS%.sdk,%,$(wildcard $(_SDK_DIR)/iPhoneOS*.sdk)))
-override SDKVERSION := $(word $(words $(_IOS_SDKS)),$(_IOS_SDKS))
+_LATEST_SDK := $(word $(words $(_IOS_SDKS)),$(_IOS_SDKS))
+
+ifeq ($(_THEOS_TARGET_SDK_VERSION),latest)
+override _THEOS_TARGET_SDK_VERSION := $(_LATEST_SDK)
 endif
 
-TARGET_IPHONEOS_DEPLOYMENT_VERSION ?= $(or $(word 2,$(_THEOS_TARGET_ARGS)),$(SDKVERSION))
+ifeq ($(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION),latest)
+override _THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION := $(_LATEST_SDK)
+endif
 
-SYSROOT ?= $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$(SDKVERSION).sdk
+SYSROOT ?= $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$(_THEOS_TARGET_SDK_VERSION).sdk
 
 TARGET_CC ?= $(SDKBINPATH)/gcc-4.2
 TARGET_CXX ?= $(SDKBINPATH)/g++-4.2
@@ -33,7 +39,7 @@ include $(THEOS_MAKE_PATH)/targets/_common/darwin.mk
 
 ARCHS ?= armv6
 
-SDKFLAGS := -isysroot $(SYSROOT) $(foreach ARCH,$(ARCHS),-arch $(ARCH)) -D__IPHONE_OS_VERSION_MIN_REQUIRED=__IPHONE_$(subst .,_,$(TARGET_IPHONEOS_DEPLOYMENT_VERSION)) -miphoneos-version-min=$(TARGET_IPHONEOS_DEPLOYMENT_VERSION)
+SDKFLAGS := -isysroot $(SYSROOT) $(foreach ARCH,$(ARCHS),-arch $(ARCH)) -D__IPHONE_OS_VERSION_MIN_REQUIRED=__IPHONE_$(subst .,_,$(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION)) -miphoneos-version-min=$(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION)
 TARGET_CFLAGS := $(SDKFLAGS)
 TARGET_LDFLAGS := $(SDKFLAGS) -multiply_defined suppress
 endif
