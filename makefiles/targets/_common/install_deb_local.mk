7b3e9cde977e31bb3b1b186329acdfff4094cf88
Ryan Petrich <rpetrich@gmail.com>
2011-07-24 10:03:03 -0400

    Fix make install with FINALPACKAGE=1

--- makefiles/targets/_common/install_deb_local.mk
+++ makefiles/targets/_common/install_deb_local.mk
@@ -2,10 +2,12 @@ export TARGET_REMOTE := 0
 
 ifeq ($(_THEOS_CAN_PACKAGE),1)
 internal-install::
+ifneq ($(FINALPACKAGE),1)
 	@if [[ "$(_THEOS_PACKAGE_LAST_VERSION)" == "none" ]]; then \
 		echo "$(MAKE) install requires that you build a package before you try to install it." >&2; \
 		exit 1; \
 	fi
+endif
 	@if [[ ! -f "$(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_FILENAME).deb" ]]; then \
 		echo "Could not find \"$(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_FILENAME).deb\" to install. Aborting." >&2; \
 		exit 1; \
