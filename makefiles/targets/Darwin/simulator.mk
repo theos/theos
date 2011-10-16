0b5b04d7ca2df0b4acff3a8533b78abcb5214c5f
Dustin L. Howett <dustin@howett.net>
2011-10-16 04:04:13 +0000

    [Darwin/simulator] Update the simulator target to use xcrun and support deployment versions like Darwin/iphone.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@430 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/targets/Darwin/simulator.mk
+++ makefiles/targets/Darwin/simulator.mk
@@ -2,23 +2,31 @@ ifeq ($(_THEOS_TARGET_LOADED),)
 _THEOS_TARGET_LOADED := 1
 THEOS_TARGET_NAME := iphone_simulator
 
-SDKBINPATH ?= $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneSimulator.platform/Developer/usr/bin
-SDKVERSION := $(or $(firstword $(_THEOS_TARGET_ARGS)),$(SDKVERSION),3.0)
+# A version specified as a target argument overrides all previous definitions.
+_SDKVERSION := $(or $(firstword $(_THEOS_TARGET_ARGS)),$(SDKVERSION))
+_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION := $(or $(word 2,$(_THEOS_TARGET_ARGS)),$(TARGET_IPHONEOS_DEPLOYMENT_VERSION),$(_SDKVERSION),3.0)
+_THEOS_TARGET_SDK_VERSION := $(or $(_SDKVERSION),latest)
 
-ifeq ($(SDKVERSION),latest)
 _SDK_DIR := $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneSimulator.platform/Developer/SDKs
 _IOS_SDKS := $(sort $(patsubst $(_SDK_DIR)/iPhoneSimulator%.sdk,%,$(wildcard $(_SDK_DIR)/iPhoneSimulator*.sdk)))
-override SDKVERSION := $(word $(words $(_IOS_SDKS)),$(_IOS_SDKS))
+_LATEST_SDK := $(word $(words $(_IOS_SDKS)),$(_IOS_SDKS))
+
+ifeq ($(_THEOS_TARGET_SDK_VERSION),latest)
+override _THEOS_TARGET_SDK_VERSION := $(_LATEST_SDK)
 endif
 
-SYSROOT ?= $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator$(SDKVERSION).sdk
+ifeq ($(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION),latest)
+override _THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION := $(_LATEST_SDK)
+endif
 
-TARGET_CC ?= $(SDKBINPATH)/gcc-4.2
-TARGET_CXX ?= $(SDKBINPATH)/g++-4.2
-TARGET_LD ?= $(SDKBINPATH)/g++-4.2
-TARGET_STRIP ?= $(SDKBINPATH)/strip
+SYSROOT ?= $(THEOS_PLATFORM_SDK_ROOT)/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator$(_THEOS_TARGET_SDK_VERSION).sdk
+
+TARGET_CC ?= xcrun -sdk iphonesimulator gcc
+TARGET_CXX ?= xcrun -sdk iphonesimulator g++
+TARGET_LD ?= xcrun -sdk iphonesimulator g++
+TARGET_STRIP ?= xcrun -sdk iphonesimulator strip
 TARGET_STRIP_FLAGS ?= -x
-TARGET_CODESIGN_ALLOCATE ?= $(SDKBINPATH)/codesign_allocate
+TARGET_CODESIGN_ALLOCATE ?= "$(shell xcrun -sdk iphonesimulator -find codesign_allocate)"
 TARGET_CODESIGN ?=
 TARGET_CODESIGN_FLAGS ?=
 
@@ -37,13 +45,13 @@ endif
 
 ARCHS ?= i386
 
-_TARGET_VERSION_GE_3_2 = $(shell $(THEOS_BIN_PATH)/vercmp.pl $(SDKVERSION) ge 3.2)
-_TARGET_VERSION_GE_4_0 = $(shell $(THEOS_BIN_PATH)/vercmp.pl $(SDKVERSION) ge 4.0)
+_TARGET_VERSION_GE_3_2 = $(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_SDK_VERSION) ge 3.2)
+_TARGET_VERSION_GE_4_0 = $(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_SDK_VERSION) ge 4.0)
 _TARGET_OSX_VERSION_FLAG = -mmacosx-version-min=$(if $(_TARGET_VERSION_GE_4_0),10.6,10.5)
 _TARGET_OBJC_ABI_CFLAGS = $(if $(_TARGET_VERSION_GE_3_2),-fobjc-abi-version=2 -fobjc-legacy-dispatch)
 _TARGET_OBJC_ABI_LDFLAGS = $(if $(_TARGET_VERSION_GE_3_2),-Xlinker -objc_abi_version -Xlinker 2)
 
-SDKFLAGS := -isysroot $(SYSROOT) $(foreach ARCH,$(ARCHS),-arch $(ARCH)) -D__IPHONE_OS_VERSION_MIN_REQUIRED=__IPHONE_$(subst .,_,$(SDKVERSION)) $(_TARGET_OSX_VERSION_FLAG)
+SDKFLAGS := -isysroot $(SYSROOT) $(foreach ARCH,$(ARCHS),-arch $(ARCH)) -D__IPHONE_OS_VERSION_MIN_REQUIRED=__IPHONE_$(subst .,_,$(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION)) $(_TARGET_OSX_VERSION_FLAG)
 
 TARGET_CFLAGS := $(SDKFLAGS) $(_TARGET_OBJC_ABI_CFLAGS)
 TARGET_LDFLAGS := $(SDKFLAGS) -multiply_defined suppress $(_TARGET_OBJC_ABI_LDFLAGS)
