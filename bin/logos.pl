7f01718e20fa712da3dd7a4b09ac774bafeb8955
Dustin L. Howett <dustin@howett.net>
2011-10-29 03:26:10 +0000

    [logos] Break some of the utility subroutines out into a new package, Logos::Util.
    git-svn-id: http://svn.howett.net/svn/theos/trunk@432 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- bin/logos.pl
+++ bin/logos.pl
@@ -8,6 +8,9 @@ use Digest::MD5 'md5_hex';
 use Module::Load;
 use Module::Load::Conditional 'can_load';
 
+use Logos::Util;
+$Logos::Util::errorhandler = \&utilErrorHandler;
+
 %main::CONFIG = ( generator => "MobileSubstrate"
 		);
 
@@ -588,25 +591,6 @@ sub generateInitLines {
 	return $return;
 }
 
-sub quotes {
-	my ($line) = @_;
-	my @quotes = ();
-	while($line =~ /(?<!\\)\"/g) {
-		push(@quotes, $-[0]);
-	}
-	return @quotes;
-}
-
-sub fallsBetween {
-	my $idx = shift;
-	while(@_ > 0) {
-		my $start = shift;
-		my $end = shift;
-		return 1 if ($start < $idx && (!defined($end) || $end > $idx))
-	}
-	return 0;
-}
-
 sub fileWarning {
 	my $curline = shift;
 	my $reason = shift;
@@ -679,87 +663,12 @@ sub nestPop {
 	return $parts[0];
 }
 
-sub sanitize {
-	my $input = shift;
-	my $output = $input;
-	$output =~ s/[^\w]//g;
-	return $output;
-}
-
 sub getGroup {
 	my $name = shift;
 	foreach(@groups) {
 		return $_ if $_->name eq $name;
 	}
 	return undef;
-}
-
-sub matchedParenthesisSet {
-	my $in = shift;
-	my $atstart = shift // 1;
-	my $opening = -1;
-	my $closing = -1;
-	if(!$atstart || $in =~ /^\s*\(/) {
-		# If we encounter a ) that puts us back at zero, we found a (
-		# and have reached its closing ).
-		my $parenmatch = $in;
-		my $pdepth = 0;
-		my @pquotes = quotes($parenmatch);
-		while($parenmatch =~ /[;()]/g) {
-			next if fallsBetween($-[0], @pquotes);
-
-			if($& eq "(") {
-				if($pdepth == 0) { $opening = $+[0]; }
-				$pdepth++;
-			} elsif($& eq ")") {
-				$pdepth--;
-				if($pdepth == 0) { $closing = $+[0]; last; }
-			}
-		}
-	}
-
-	return undef if $opening == -1;
-	fileError($lineno, "missing closing parenthesis") if $closing == -1;
-	return ($opening, $closing);
-}
-
-sub nestedParenString {
-	my $in = shift;
-	my ($opening, $closing) = matchedParenthesisSet($in);
-
-	my @ret;
-	if(defined $opening) {
-		$ret[0] = substr($in, $opening, $closing - $opening - 1);
-		$in = substr($in, $closing);
-	}
-	$ret[1] = $in;
-	return @ret;
-}
-
-sub smartSplit {
-	my $re = shift;
-	my $in = shift;
-	return () if $in eq "";
-
-	my $limit = shift // 0;
-
-	my @quotes = quotes($in);
-	my @parens = matchedParenthesisSet($in, 0);
-
-	my $lstart = 0;
-	my @pieces = ();
-	my $piece = "";
-	while($in =~ /$re/g) {
-		next if (defined $parens[0] && fallsBetween($-[0], @parens)) || fallsBetween($-[0], @quotes);
-		$piece = substr($in, $lstart, $-[0]-$lstart);
-		push(@pieces, $piece);
-		$lstart = $+[0];
-		$limit--;
-		last if($limit == 1); # One item left? Bail out and throw the rest of the string into it!
-	}
-	$piece = substr($in, $lstart);
-	push(@pieces, $piece);
-	return @pieces;
 }
 
 sub lookupLineMapping {
@@ -788,4 +697,8 @@ sub lineDescriptionForPhysicalLine {
 	my $filename = $lineMap[0];
 	my $lineno = $lineMap[1];
 	return "$filename:$lineno";
+}
+
+sub utilErrorHandler {
+	fileError($lineno, shift);
 }
