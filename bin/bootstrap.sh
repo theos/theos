90b360a2c11ee853e996149c94aa2fdc4bb4c8f4
Dustin L. Howett <dustin@howett.net>
2011-12-30 06:44:43 +0000

    [bootstrap] Fix the bootstrap-generated header for non-C++ source.
    From: Dustin L. Howett <dustin@howett.net>
    
    git-svn-id: http://svn.howett.net/svn/theos/trunk@486 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -105,7 +105,10 @@ function makeSubstrateHeader() {
 #include <sys/types.h>
 #include <objc/runtime.h>
 #ifdef __cplusplus
+#define _default(x) = x
 extern "C" {
+#else
+#define _default(x)
 #endif
 typedef const void *MSImageRef;
 void MSHookFunction(void *symbol, void *replace, void **result);
@@ -114,7 +117,7 @@ MSImageRef MSGetImageByName(const char *file);
 
 #ifdef __APPLE__
 #ifdef __arm__
-IMP MSHookMessage(Class _class, SEL sel, IMP imp, const char *prefix = NULL);
+IMP MSHookMessage(Class _class, SEL sel, IMP imp, const char *prefix _default(NULL));
 #endif
 void MSHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result);
 #endif
