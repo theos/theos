bad661f12e512f168d5d7a358d26c8b12e5542cb
Dustin L. Howett <dustin@howett.net>
2012-08-30 07:49:10 +0000

    [nic] Rewrite nicify to generate and lint-check NICTar-format templates. Additionally, it now writes its own output file and does not require redirection.
    From: Dustin L. Howett <dustin@howett.net>
    
    git-svn-id: http://svn.howett.net/svn/theos/trunk@543 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- bin/nicify.pl
+++ bin/nicify.pl
@@ -1,64 +1,116 @@
 #!/usr/bin/perl
 
+use strict;
 use warnings;
 use File::Find;
+use File::Spec;
+use Archive::Tar;
+use Cwd qw(abs_path getcwd);
 
-@directories = ();
-%files = ();
-%symlinks = ();
+use FindBin;
+use lib "$FindBin::Bin/lib";
+use NIC::Formats::NICTar;
+
+package NIC::Archive::Tar::File;
+use parent "Archive::Tar::File";
+sub new {
+	my $class = shift;
+	my $self = Archive::Tar::File->new(@_);
+	bless($self, $class);
+	return $self;
+}
+
+sub _prefix_and_file {
+	my $self = shift;
+	my $path = shift;
+	my ($prefix, $file) = $self->SUPER::_prefix_and_file($path);
+	$prefix =~ s/^/\.\// if $prefix ne "" && $prefix ne "." && $prefix !~ /^\.\//;
+	return ($prefix, $file);
+}
+1;
+package main;
+
+my $cwd = abs_path(getcwd());
+my $tar = Archive::Tar->new();
+my $controlfile = undef;
+our @tarfiles = (
+	NIC::Archive::Tar::File->new(data=>"./", "", {type=>Archive::Tar::Constant::DIR, uid=>0, gid=>0, mode=>0755}),
+	NIC::Archive::Tar::File->new(data=>"./NIC/", "", {type=>Archive::Tar::Constant::DIR, uid=>0, gid=>0, mode=>0777})
+);
 
 chdir $ARGV[0];
-find({wanted => \&processDirectories, follow => 0, no_chdir => 1}, ".");
-find({wanted => \&processFiles, follow => 0, no_chdir => 1}, ".");
-find({wanted => \&processSymlinks, follow => 0, no_chdir => 1}, ".");
 
-print "nic 1",$/;
+my $control_in = undef;
 
 if(-f "pre.NIC") {
-	open(my $pfh, "<", "pre.NIC");
-	while(<$pfh>) {
-		print $_;
+	warning("Using legacy pre.NIC as ./NIC/control.");
+	$control_in = "./pre.NIC";
+} elsif(-f "NIC/control") {
+	$control_in = "./NIC/control";
+}
+
+if(!$control_in) {
+	exitWithError("No control file found at NIC/control.");
+	exit 1;
+}
+
+$controlfile = NIC::Archive::Tar::File->new(file=>$control_in);
+$controlfile->prefix("./NIC");
+$controlfile->name("control");
+push(@tarfiles, $controlfile);
+
+find({wanted => \&wanted, follow => 0, no_chdir => 1}, ".");
+
+$tar->add_files(@tarfiles);
+
+chdir($cwd);
+my $newnic = NIC::Formats::NICTar->new($tar);
+if(!defined $newnic->name) {
+	exitWithError("Template has no name. Please insert a `name \"<name>\"` directive into $control_in.");
+}
+
+{ my $_ = scalar @{$newnic->{CONTENTS}}; info("$_ entr".($_==1?"y.":"ies.")); }
+{ my $_ = scalar @{$newnic->{PROMPTS}}; info("$_ prompt".($_==1?".":"s.")); }
+my $constraints = 0;
+{
+	my %constrainthash;
+	for(@{$newnic->{CONTENTS}}) {
+		for my $c ($_->constraints) {
+			$constrainthash{$c}++;
+		}
 	}
-	close($pfh);
+	$constraints = scalar keys %constrainthash;
 }
+{ my $_ = $constraints; info("$_ constraint".($_==1?".":"s.")); }
 
-foreach $dir (@directories) {
-	print "dir $dir",$/;
-}
+my $fixedfn = join("_", File::Spec->splitdir($newnic->name));
+my $filename = $fixedfn.".nic.tar";
+$tar->write($filename) and info("Archived template \"".$newnic->name."\" to $filename.");
 
-foreach $filename (keys %files) {
-	my @lines = split(/\n/, $files{$filename});
-	my $ln = scalar(@lines);
-	print "file $ln $filename",$/;
-	print $files{$filename},$/;
-}
-
-foreach $symlink (keys %symlinks) {
-	print "symlink \"$symlink\" \"".$symlinks{$symlink}."\"",$/;
-}
-
-sub processDirectories {
-	return if(! -d $_);
+sub wanted {
+	local $_ = $File::Find::name;
 	return if(/\.svn/);
-	return if(/^.$/);
-	s/^\.\///;
-	push(@directories, $_);
-}
+	my $mode = (stat($_))[2];
 
-sub processFiles {
-	return if(! -f $_ || -l $_);
-	return if(/\.svn/);
-	return if(/\.[Nn][Ii][Cc]$/);
-	s/^\.\///;
-	$files{$_} = slurp($_);
-}
-
-sub processSymlinks {
-	return if(! -l $_);
-	return if(/\.svn/);
-	return if(/\.nic$/);
-	s/^\.\///;
-	$symlinks{$_} = readlink($_);
+	my $tarfile = undef;
+	if(-d $_) {
+		s/$/\// if !/\/$/;
+		return if /^\.\/$/;
+		return if /^\.\/NIC\/?$/;
+		$tarfile = NIC::Archive::Tar::File->new(data=>$_, "", {mode=>$mode, uid=>0, gid=>0, type=>Archive::Tar::Constant::DIR});
+	} elsif(-f $_ && ! -l $_) {
+		return if(/\.[Nn][Ii][Cc]$/);
+		return if /^\.\/NIC\/control$/;
+		return if /\.nic\.tar$/;
+		$tarfile = NIC::Archive::Tar::File->new(file=>$_);
+		$tarfile->mode($mode);
+		$tarfile->uid(0);
+		$tarfile->gid(0);
+	} elsif(-l $_) {
+		return if(/\.[Nn][Ii][Cc]$/);
+		$tarfile = NIC::Archive::Tar::File->new(data=>$_, "", {linkname=>readlink($_), uid=>0, gid=>0, type=>Archive::Tar::Constant::SYMLINK});
+	}
+	push(@tarfiles, $tarfile) if $tarfile;
 }
 
 sub slurp {
@@ -68,3 +120,20 @@ sub slurp {
 	my $d = <$fh>;
 	return $d;
 }
+
+sub info {
+	my $text = shift;
+	print STDERR "[info] ", $text, $/;
+}
+
+sub warning {
+	my $text = shift;
+	print STDERR "[warning] ", $text, $/;
+}
+
+sub exitWithError {
+	my $error = shift;
+	print STDERR "[error] ", $error, $/;
+	exit 1;
+}
+
