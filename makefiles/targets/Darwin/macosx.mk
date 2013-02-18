2dadabebee3a43c02b5cd53cc06e00ca81d00d19
Conrad Kramer <conrad@kramerapps.com>
2013-02-18 01:17:26 -0500

    [Darwin/macosx] Add support for the optional :clang argument

--- makefiles/targets/Darwin/macosx.mk
+++ makefiles/targets/Darwin/macosx.mk
@@ -2,10 +2,20 @@ ifeq ($(_THEOS_TARGET_LOADED),)
 _THEOS_TARGET_LOADED := 1
 THEOS_TARGET_NAME := macosx
 
-_THEOS_TARGET_MACOSX_DEPLOYMENT_VERSION := $(__THEOS_TARGET_ARG_1)
-TARGET_CC ?= xcrun -sdk macosx gcc
-TARGET_CXX ?= xcrun -sdk macosx g++
-TARGET_LD ?= xcrun -sdk macosx g++
+ifeq ($(__THEOS_TARGET_ARG_1),clang)
+_THEOS_TARGET_CC := clang
+_THEOS_TARGET_CXX := clang++
+_THEOS_TARGET_ARG_ORDER := 2
+else
+_THEOS_TARGET_CC := gcc
+_THEOS_TARGET_CXX := g++
+_THEOS_TARGET_ARG_ORDER := 1
+endif
+
+_THEOS_TARGET_MACOSX_DEPLOYMENT_VERSION := $(__THEOS_TARGET_ARG_$(_THEOS_TARGET_ARG_ORDER))
+TARGET_CC ?= xcrun -sdk macosx $(_THEOS_TARGET_CC)
+TARGET_CXX ?= xcrun -sdk macosx $(_THEOS_TARGET_CXX)
+TARGET_LD ?= xcrun -sdk macosx $(_THEOS_TARGET_CXX)
 TARGET_STRIP ?= xcrun -sdk macosx strip
 TARGET_STRIP_FLAGS ?= -x
 TARGET_CODESIGN_ALLOCATE ?= "$(shell xcrun -sdk macosx -find codesign_allocate)"
