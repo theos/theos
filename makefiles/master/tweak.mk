85c0d6619e17ea47e0e079d594d0234530284206
Ryan Petrich <rpetrich@gmail.com>
2013-03-06 17:11:39 -0700

    Add TWEAK_TARGET_PROCESS define that can be used to select which processes to kill when installing

--- makefiles/master/tweak.mk
+++ makefiles/master/tweak.mk
@@ -4,6 +4,8 @@ ifeq ($(_THEOS_RULES_LOADED),)
 include $(THEOS_MAKE_PATH)/rules.mk
 endif
 
+TWEAK_TARGET_PROCESS ?= SpringBoard backboardd
+
 before-all::
 	@[ -f "$(THEOS_LIBRARY_PATH)/libsubstrate.dylib" ] || bootstrap.sh substrate
 
@@ -12,7 +14,7 @@ internal-all:: $(TWEAK_NAME:=.all.tweak.variables);
 internal-stage:: $(TWEAK_NAME:=.stage.tweak.variables);
 
 internal-after-install::
-	install.exec "timeout 10s sbreload || ( ( respring || killall -9 SpringBoard ) && launchctl load /System/Library/LaunchDaemons/com.apple.SpringBoard.plist )"
+	install.exec "killall -9 $(TWEAK_TARGET_PROCESS)"
 
 TWEAKS_WITH_SUBPROJECTS = $(strip $(foreach tweak,$(TWEAK_NAME),$(patsubst %,$(tweak),$(call __schema_var_all,$(tweak)_,SUBPROJECTS))))
 ifneq ($(TWEAKS_WITH_SUBPROJECTS),)
