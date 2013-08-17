331969e623eae55b4e854cfa7f8b92288aed1fb0
Dustin L. Howett <dustin@howett.net>
2013-08-17 08:49:05 +0000

    [makefiles] Remove bashisms.
    From: Dustin L. Howett <dustin@howett.net>
    
    git-svn-id: http://svn.howett.net/svn/theos/trunk@607 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- makefiles/targets/_common/install_deb_local.mk
+++ makefiles/targets/_common/install_deb_local.mk
@@ -2,11 +2,11 @@ export TARGET_REMOTE := 0
 
 ifeq ($(_THEOS_CAN_PACKAGE),$(_THEOS_TRUE))
 internal-install::
-	@if [[ "$(_THEOS_PACKAGE_LAST_VERSION)" == "none" ]]; then \
+	@if [ "$(_THEOS_PACKAGE_LAST_VERSION)" = "none" ]; then \
 		echo "$(MAKE) install requires that you build a package before you try to install it." >&2; \
 		exit 1; \
 	fi
-	@if [[ ! -f "$(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_FILENAME).deb" ]]; then \
+	@if [ ! -f "$(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_FILENAME).deb" ]; then \
 		echo "Could not find \"$(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_FILENAME).deb\" to install. Aborting." >&2; \
 		exit 1; \
 	fi
