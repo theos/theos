ffddc614c54c2707602341a8b2fac0b9d540d9d3
Dustin L. Howett <dustin@howett.net>
2013-10-15 22:47:46 +0000

    [logos] Dollar signs are valid in *all* parts of a selector. Not just the non-first parts.
    From: Dustin L. Howett <dustin@howett.net>
    
    git-svn-id: http://svn.howett.net/svn/theos/trunk@630 4410221e-0ddf-4ce3-99c0-2db6c0dbc727

--- bin/logos.pl
+++ bin/logos.pl
@@ -319,7 +319,7 @@ foreach my $line (@lines) {
 			$xtype = $2 if $2;
 			$newMethodTypeEncoding = $xtype;
 			patchHere(undef);
-		} elsif($currentClass && $line =~ /\G([+-])\s*\(\s*(.*?)\s*\)(?=\s*[\w:])/gc && $directiveDepth < 1) {
+		} elsif($currentClass && $line =~ /\G([+-])\s*\(\s*(.*?)\s*\)(?=\s*[\$\w:])/gc && $directiveDepth < 1) {
 			# [+-] (<return>)<[X:]>, but only when we're in a %hook.
 
 			# Gasp! We've been moved to a different group!
