f0108c40529d0ad0d518f127b5664094d5abb138
CoolStar <coolstarorganization@gmail.com>
2014-08-04 10:46:37 -0700

    Add support for an updated iOS toolchain for Linux.

--- makefiles/targets/Linux/iphone.mk
+++ makefiles/targets/Linux/iphone.mk
@@ -2,29 +2,73 @@ ifeq ($(_THEOS_TARGET_LOADED),)
 _THEOS_TARGET_LOADED := 1
 THEOS_TARGET_NAME := iphone
 
-SDKTARGET ?= arm-apple-darwin9
-SDKBINPATH ?= /opt/iphone-sdk-3.0/prefix/bin
-SYSROOT ?= /opt/iphone-sdk-3.0/sysroot
+# A version specified as a target argument overrides all previous definitions.
+SDKTARGET ?= armv7-apple-darwin11
+SDKBINPATH ?= theos/toolchain/$(THEOS_PLATFORM_NAME)/$(THEOS_TARGET_NAME)/bin
+
+_THEOS_TARGET_CC := clang
+_THEOS_TARGET_CXX := clang++
+
+_SDKVERSION := $(or $(__THEOS_TARGET_ARG_$(word 1,$(_THEOS_TARGET_ARG_ORDER))),$(SDKVERSION))
+_THEOS_TARGET_SDK_VERSION := $(or $(_SDKVERSION),latest)
+
+_SDK_DIR := $(THEOS)/sdks
+_IOS_SDKS := $(sort $(patsubst $(_SDK_DIR)/iPhoneOS%.sdk,%,$(wildcard $(_SDK_DIR)/iPhoneOS*.sdk)))
+_LATEST_SDK := $(word $(words $(_IOS_SDKS)),$(_IOS_SDKS))
+
+ifeq ($(_THEOS_TARGET_SDK_VERSION),latest)
+override _THEOS_TARGET_SDK_VERSION := $(_LATEST_SDK)
+endif
+
+# We have to figure out the target version here, as we need it in the calculation of the deployment version.
+_TARGET_VERSION_GE_6_0 = $(call __simplify,_TARGET_VERSION_GE_6_0,$(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_SDK_VERSION) ge 6.0))
+_TARGET_VERSION_GE_3_0 = $(call __simplify,_TARGET_VERSION_GE_3_0,$(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_SDK_VERSION) ge 3.0))
+_TARGET_VERSION_GE_4_0 = $(call __simplify,_TARGET_VERSION_GE_4_0,$(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_SDK_VERSION) ge 4.0))
+_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION := $(or $(__THEOS_TARGET_ARG_$(word 2,$(_THEOS_TARGET_ARG_ORDER))),$(TARGET_IPHONEOS_DEPLOYMENT_VERSION),$(_SDKVERSION),3.0)
+
+ifeq ($(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION),latest)
+override _THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION := $(_LATEST_SDK)
+endif
+
+_DEPLOY_VERSION_GE_3_0 = $(call __simplify,_DEPLOY_VERSION_GE_3_0,$(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION) ge 3.0))
+_DEPLOY_VERSION_LT_4_3 = $(call __simplify,_DEPLOY_VERSION_LT_4_3,$(shell $(THEOS_BIN_PATH)/vercmp.pl $(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION) lt 4.3))
+
+ifeq ($(_TARGET_VERSION_GE_6_0)$(_DEPLOY_VERSION_GE_3_0)$(_DEPLOY_VERSION_LT_4_3),111)
+ifeq ($(_THEOS_TARGET_WARNED_DEPLOY),)
+$(warning Deploying to iOS 3.0 while building for 6.0 will generate armv7-only binaries.)
+export _THEOS_TARGET_WARNED_DEPLOY := 1
+endif
+endif
+
+SYSROOT ?= $(THEOS)/sdks/iPhoneOS$(_THEOS_TARGET_SDK_VERSION).sdk
 
 PREFIX := $(SDKBINPATH)/$(SDKTARGET)-
 
-TARGET_CC ?= $(PREFIX)gcc
-TARGET_CXX ?= $(PREFIX)g++
-TARGET_LD ?= $(PREFIX)g++
+TARGET_CC ?= $(PREFIX)$(_THEOS_TARGET_CC)
+TARGET_CXX ?= $(PREFIX)$(_THEOS_TARGET_CXX)
+TARGET_LD ?= $(PREFIX)$(_THEOS_TARGET_CXX)
 TARGET_STRIP ?= $(PREFIX)strip
 TARGET_STRIP_FLAGS ?= -x
 TARGET_CODESIGN_ALLOCATE ?= $(PREFIX)codesign_allocate
-TARGET_CODESIGN ?= ldid
+TARGET_CODESIGN ?= $(SDKBINPATH)/ldid
 TARGET_CODESIGN_FLAGS ?= -S
+
+TARGET_PRIVATE_FRAMEWORK_PATH = $(SYSROOT)/System/Library/PrivateFrameworks
 
 include $(THEOS_MAKE_PATH)/targets/_common/darwin.mk
 include $(THEOS_MAKE_PATH)/targets/_common/darwin_flat_bundle.mk
 
-TARGET_PRIVATE_FRAMEWORK_PATH = $(SYSROOT)/System/Library/PrivateFrameworks
+ifeq ($(_TARGET_VERSION_GE_6_0),1) # >= 6.0 {
+	ARCHS ?= armv7
+else # } < 6.0 {
+	ARCHS ?= armv6 armv7
+endif # }
 
-SDKFLAGS := -isysroot $(SYSROOT)
+SDKFLAGS := -isysroot "$(SYSROOT)" $(foreach ARCH,$(ARCHS),-arch $(ARCH)) -miphoneos-version-min=$(_THEOS_TARGET_IPHONEOS_DEPLOYMENT_VERSION)
 _THEOS_TARGET_CFLAGS := $(SDKFLAGS)
 _THEOS_TARGET_LDFLAGS := $(SDKFLAGS) -multiply_defined suppress
+#stupid llvm configuring the linker version incorrectly
+_THEOS_TARGET_LDFLAGS := $(_THEOS_TARGET_LDFLAGS) -mlinker-version=128.2
 
 TARGET_INSTALL_REMOTE := $(_THEOS_TRUE)
 _THEOS_TARGET_DEFAULT_PACKAGE_FORMAT := deb
