f9ffc28e7ff241e608ca6d012a35b63fd65e1c46
uroboro <uroboro845@gmail.com>
2015-01-27 14:27:31 -0300

    Added %dtor directive

--- bin/logos.pl
+++ bin/logos.pl
@@ -469,6 +469,12 @@ if (!defined $currentClass) {
 			nestingMustNotContain($lineno, "%ctor", \@nestingstack, "hook", "subclass");
 			my $replacement = "static __attribute__((constructor)) void _logosLocalCtor_".substr(md5_hex($`.$lineno.$'), 0, 8)."()";
 			patchHere($replacement);
+		} elsif($line =~ /\G%dtor\b/gc) {
+			# %dtor
+			fileError($lineno, "%dtor does not make sense inside a block") if($directiveDepth >= 1);
+			nestingMustNotContain($lineno, "%dtor", \@nestingstack, "hook", "subclass");
+			my $replacement = "static __attribute__((destructor)) void _logosLocalDtor_".substr(md5_hex($`.$lineno.$'), 0, 8)."()";
+			patchHere($replacement);
 		} elsif($line =~ /\G%init\b/gc) {
 			# %init, with optional following parens
 			fileError($lineno, "%init does not make sense outside a block") if($directiveDepth < 1);
