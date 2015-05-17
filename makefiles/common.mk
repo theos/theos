245615025410c3b2a21b6c3500d380af5402fb9e
Adam Demasi <kirbylover4000@gmail.com>
2015-05-17 18:50:13 +0930

    [common] Flip $(DEBUG), so debug must be disabled when unwanted.

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -52,7 +52,7 @@ export PATH := $(THEOS_BIN_PATH):$(PATH)
 ifeq ($(THEOS_SCHEMA),)
 _THEOS_SCHEMA := $(shell echo "$(strip $(schema) $(SCHEMA))" | tr 'a-z' 'A-Z')
 _THEOS_ON_SCHEMA := DEFAULT $(filter-out -%,$(_THEOS_SCHEMA))
-ifeq ($(call __theos_bool,$(or $(debug),$(DEBUG))),$(_THEOS_TRUE))
+ifneq ($(call __theos_bool,$(or $(debug),$(DEBUG))),$(_THEOS_FALSE))
 	_THEOS_ON_SCHEMA += DEBUG
 endif
 _THEOS_OFF_SCHEMA := $(patsubst -%,%,$(filter -%,$(_THEOS_SCHEMA)))
