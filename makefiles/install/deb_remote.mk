3b870b667b283a110e47a600e47f9c394c7c14e4
Adam Demasi <kirbylover4000@gmail.com>
2015-07-07 03:21:36 +0930

    [package] MobileCydia decided to be Cydia now.

--- makefiles/install/deb_remote.mk
+++ makefiles/install/deb_remote.mk
@@ -7,4 +7,4 @@ internal-install:: internal-install-check
 		echo "Could not find \"$(_THEOS_PACKAGE_LAST_FILENAME)\" to install. Aborting." >&2; \
 		exit 1; \
 	fi
-	install.exec "killall MobileCydia 2> /dev/null; cat > /tmp/_theos_install.deb; $(THEOS_SUDO_COMMAND) dpkg -i /tmp/_theos_install.deb && rm /tmp/_theos_install.deb" < "$(_THEOS_PACKAGE_LAST_FILENAME)"
+	install.exec "killall Cydia 2> /dev/null; cat > /tmp/_theos_install.deb; $(THEOS_SUDO_COMMAND) dpkg -i /tmp/_theos_install.deb && rm /tmp/_theos_install.deb" < "$(_THEOS_PACKAGE_LAST_FILENAME)"
