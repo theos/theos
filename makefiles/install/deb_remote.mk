454e69f9c73fd32358d5549e7027a750b059862c
Adam Demasi <adam@hbang.com.au>
2015-08-02 17:20:14 +0930

    [install] Remove now duplicated Cydia kill.

--- makefiles/install/deb_remote.mk
+++ makefiles/install/deb_remote.mk
@@ -1,3 +1,3 @@
 internal-install:: internal-install-check
 	$(ECHO_INSTALLING)true$(ECHO_END)
-	$(ECHO_NOTHING)install.exec "killall Cydia 2> /dev/null; cat > /tmp/_theos_install.deb; $(THEOS_SUDO_COMMAND) dpkg -i /tmp/_theos_install.deb && rm /tmp/_theos_install.deb" < "$(_THEOS_PACKAGE_LAST_FILENAME)"$(ECHO_END)
+	$(ECHO_NOTHING)install.exec "cat > /tmp/_theos_install.deb; $(THEOS_SUDO_COMMAND) dpkg -i /tmp/_theos_install.deb && rm /tmp/_theos_install.deb" < "$(_THEOS_PACKAGE_LAST_FILENAME)"$(ECHO_END)
