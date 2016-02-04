efde4e03ea0e74b7735264ce1f0a188c183f0497
Karen Tsai <angelXwind@angelxwind.net>
2016-02-03 19:09:49 -0500

    [platform] Add platform-specific paths for lipo.

--- makefiles/platform/Linux.mk
+++ makefiles/platform/Linux.mk
@@ -5,5 +5,8 @@ THEOS_PLATFORM_NAME := linux
 _THEOS_PLATFORM_DEFAULT_TARGET := iphone
 _THEOS_PLATFORM_DU_EXCLUDE := --exclude
 _THEOS_PLATFORM_MD5SUM := md5sum
+# TODO: Figure out if hardcoding "/iphone/" in _THEOS_PLATFORM_LIPO's path is a good idea or not
+_THEOS_PLATFORM_LIPO = $(THEOS)/toolchain/$(THEOS_PLATFORM_NAME)/iphone/bin/armv7-apple-darwin11-lipo
+# TODO: Find some better way to determine _THEOS_PLATFORM_SHOW_IN_FILE_MANAGER, as not all desktop environments use Nautilus as the file manager
 _THEOS_PLATFORM_SHOW_IN_FILE_MANAGER := nautilus
 endif
--- makefiles/platform/Windows.mk
+++ makefiles/platform/Windows.mk
@@ -6,5 +6,7 @@ _THEOS_PLATFORM_DEFAULT_TARGET := iphone
 _THEOS_PLATFORM_DU_EXCLUDE := --exclude
 _THEOS_PLATFORM_DPKG_DEB := dm.pl
 _THEOS_PLATFORM_MD5SUM := md5sum
+# TODO: Figure out if hardcoding "/iphone/" in _THEOS_PLATFORM_LIPO's path is a good idea or not
+_THEOS_PLATFORM_LIPO = $(THEOS)/toolchain/$(THEOS_PLATFORM_NAME)/iphone/bin/armv7-apple-darwin11-lipo
 _THEOS_PLATFORM_SHOW_IN_FILE_MANAGER := explorer /select,
 endif
