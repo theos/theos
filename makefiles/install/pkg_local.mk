252c1890fb341bc18d42297ad66635165ef3b78b
Ryan Petrich <rpetrich@gmail.com>
2016-04-09 17:41:58 -0400

    Support OS X's packaging format

---
+++ makefiles/install/pkg_local.mk
@@ -0,0 +1,11 @@
+internal-install::
+	$(ECHO_INSTALLING)true$(ECHO_END)
+	@if [ -z "$(_THEOS_PACKAGE_LAST_FILENAME)" ]; then \
+		echo "$(MAKE) install requires that you build a package before you try to install it." >&2; \
+		exit 1; \
+	fi
+	@if [ ! -f "$(_THEOS_PACKAGE_LAST_FILENAME)" ]; then \
+		echo "Could not find \"$(_THEOS_PACKAGE_LAST_FILENAME)\" to install. Aborting." >&2; \
+		exit 1; \
+	fi
+	$(ECHO_NOTHING)install.exec "$(THEOS_SUDO_COMMAND) sudo installer -pkg \"$(_THEOS_PACKAGE_LAST_FILENAME)\" -target /"$(ECHO_END)
---
+++ makefiles/install/pkg_remote.mk
@@ -0,0 +1,11 @@
+internal-install::
+	$(ECHO_INSTALLING)true$(ECHO_END)
+	@if [ -z "$(_THEOS_PACKAGE_LAST_FILENAME)" ]; then \
+		echo "$(MAKE) install requires that you build a package before you try to install it." >&2; \
+		exit 1; \
+	fi
+	@if [ ! -f "$(_THEOS_PACKAGE_LAST_FILENAME)" ]; then \
+		echo "Could not find \"$(_THEOS_PACKAGE_LAST_FILENAME)\" to install. Aborting." >&2; \
+		exit 1; \
+	fi
+	$(ECHO_NOTHING)install.exec "cat > /tmp/_theos_install.pkg; $(THEOS_SUDO_COMMAND) sudo installer -pkg /tmp/_theos_install.pkg && rm /tmp/_theos_install.pkg" < "$(_THEOS_PACKAGE_LAST_FILENAME)"$(ECHO_END)
