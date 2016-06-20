a143cc74113f5b7074b1b2c8dc08cfb05cdf4ed9
Adam Demasi <adam@hbang.com.au>
2016-06-21 01:26:40 +0930

    [install] Somehow, we lost the underscore on _THEOS_SUDO_COMMAND.
    This caused sudo to be used again where it shouldn't... closes #164.

--- makefiles/install/deb_local.mk
+++ makefiles/install/deb_local.mk
@@ -1,5 +1,5 @@
 internal-install:: internal-install-check
-	$(ECHO_INSTALLING)install.exec "$(THEOS_SUDO_COMMAND) dpkg -i \"$(_THEOS_PACKAGE_LAST_FILENAME)\""$(ECHO_END)
+	$(ECHO_INSTALLING)install.exec "$(_THEOS_SUDO_COMMAND) dpkg -i \"$(_THEOS_PACKAGE_LAST_FILENAME)\""$(ECHO_END)
 
 internal-uninstall::
-	$(ECHO_NOTHING)install.exec "$(THEOS_SUDO_COMMAND) dpkg -r \"$(THEOS_PACKAGE_NAME)\""$(ECHO_END)
+	$(ECHO_NOTHING)install.exec "$(_THEOS_SUDO_COMMAND) dpkg -r \"$(THEOS_PACKAGE_NAME)\""$(ECHO_END)
