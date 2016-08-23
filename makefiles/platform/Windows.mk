345360b888ccc6fdf7341eeed4f0ee430848c61f
Adam Demasi <adam@hbang.com.au>
2016-08-24 08:24:01 +0930

    [Windows] We should have been setting compression to gzip for dm.pl.

--- makefiles/platform/Windows.mk
+++ makefiles/platform/Windows.mk
@@ -5,6 +5,7 @@ THEOS_PLATFORM_NAME := windows
 _THEOS_PLATFORM_DEFAULT_TARGET := iphone
 _THEOS_PLATFORM_DU_EXCLUDE := --exclude
 _THEOS_PLATFORM_DPKG_DEB := dm.pl
+_THEOS_PLATFORM_DPKG_DEB_COMPRESSION := gzip
 _THEOS_PLATFORM_MD5SUM := md5sum
 # TODO: Figure out if hardcoding "/iphone/" in _THEOS_PLATFORM_LIPO's path is a good idea or not
 _THEOS_PLATFORM_LIPO = $(THEOS)/toolchain/$(THEOS_PLATFORM_NAME)/iphone/bin/armv7-apple-darwin11-lipo
