454dc9ccfa0c888873d3c04f2807abfcce9c1b6f
uroboro <uroboro845@gmail.com>
2016-12-15 21:14:24 -0300

    [logos] Dynamic lookup of functions generation.

--- bin/lib/Logos/Generator/Base/Function.pm
+++ bin/lib/Logos/Generator/Base/Function.pm
@@ -3,16 +3,23 @@ use strict;
 use Logos::Generator;
 use Logos::Util;
 
+sub getLiteralOrLookupFunctionName {
+	my $self = shift;
+	my $name = shift;
+	return "lookup\$".substr($name, 1, -1) if (substr($name, 0, 1) eq "\"" && substr($name, -1, 1) eq "\"");
+	return $name;
+}
+
 sub originalFunctionName {
 	my $self = shift;
 	my $function = shift;
-	return Logos::sigil("orig").$function->group->name."\$".$function->name;
+	return Logos::sigil("orig").$function->group->name."\$".$self->getLiteralOrLookupFunctionName($function->name);
 }
 
 sub newFunctionName {
 	my $self = shift;
 	my $function = shift;
-	return Logos::sigil("function").$function->group->name."\$".$function->name;
+	return Logos::sigil("function").$function->group->name."\$".$self->getLiteralOrLookupFunctionName($function->name);
 }
 
 sub originalFunctionCall {
