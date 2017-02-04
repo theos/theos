9b640225b9acabace25c2b624a6f7b54520d9c8c
CoolStar <coolstarorganization@gmail.com>
2017-02-03 23:12:57 -0800

    Update Windows/Cygwin makefiles for new iOS 10 toolchain.

--- makefiles/platform/Windows.mk
+++ makefiles/platform/Windows.mk
@@ -8,6 +8,6 @@ _THEOS_PLATFORM_DPKG_DEB := dm.pl
 _THEOS_PLATFORM_DPKG_DEB_COMPRESSION := gzip
 _THEOS_PLATFORM_MD5SUM := md5sum
 # TODO: Figure out if hardcoding "/iphone/" in _THEOS_PLATFORM_LIPO's path is a good idea or not
-_THEOS_PLATFORM_LIPO = $(THEOS)/toolchain/$(THEOS_PLATFORM_NAME)/iphone/bin/armv7-apple-darwin11-lipo
+_THEOS_PLATFORM_LIPO = $(THEOS)/toolchain/$(THEOS_PLATFORM_NAME)/iphone/bin/arm64-apple-darwin14-lipo
 _THEOS_PLATFORM_SHOW_IN_FILE_MANAGER := explorer /select,
 endif
