1d41e0109bb8bce0f6355ec55bf28be76bf6167e
uroboro <uroboro845@gmail.com>
2017-06-18 23:52:41 -0300

    [Logos] Fix %property parsing of type and name.
    Fixes issue #226.

--- bin/logos.pl
+++ bin/logos.pl
@@ -563,7 +563,7 @@ foreach my $line (@lines) {
 		} elsif($line =~ /\G%config\s*\(\s*(\w+)\s*=\s*(.*?)\s*\)/gc) {
 			$main::CONFIG{$1} = $2;
 			patchHere(undef);
-		} elsif($line =~ /\G%property\s*(?:\((\s*\w+\s*(?:,\s*(?:\w|\=|:)+\s*)*)\))?\s*((?:\w+\s+\**)+)(\w+)\s*;/gc){
+		} elsif($line =~ /\G%property\s*(?:\((\s*\w+\s*(?:,\s*(?:\w|\=|:)+\s*)*)\))?\s*([a-zA-Z_\$][\w\$]*(?:\s*[a-zA-Z_\$][\w\$]*)*(?:\s*\*+)*)\s*(\b[a-zA-Z_\$][\w\$]*+)\s*;/gc){
 			nestingMustContain($lineno, "%property", \@nestingstack, "hook", "subclass");
 
 			$currentClass->hasinstancehooks(1);
