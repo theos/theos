dfd75c5798f431b67f523d65af83a4644bb407d7
Sam Bingner <sam@bingner.com>
2018-03-18 23:29:09 -1000

    Find absolute path to dm.pl from path to support non-vendor versions and use dm.pl on iOS devices only when necessary modules are present

--- makefiles/platform/Darwin-arm.mk
+++ makefiles/platform/Darwin-arm.mk
@@ -3,7 +3,12 @@ _THEOS_PLATFORM_LOADED := 1
 THEOS_PLATFORM_NAME := iphone
 
 _THEOS_PLATFORM_DEFAULT_TARGET := iphone
-_THEOS_PLATFORM_DPKG_DEB := dpkg-deb
+_THEOS_PLATFORM_DPKG_DEB := $(shell \
+	PATH=$$PATH:$(THEOS_BIN_PATH); \
+	DM=$$(which dm.pl); \
+	[ ! -z "$${DM}" ] && $(PERL) -MIO::Compress::Lzma -MIO::Compress::Gzip -MIO::Compress::Bzip2 -MIO::Compress::Xz -e 'exit 0' \
+		&& echo $$DM || echo dpkg-deb \
+	)
 _THEOS_PLATFORM_DU_EXCLUDE := --exclude
 _THEOS_PLATFORM_MD5SUM := md5sum
 _THEOS_PLATFORM_LIPO = lipo
