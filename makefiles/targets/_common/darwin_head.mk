b49f85c8f3c4aaade144d7abf28e633aebe5c89f
Adam Demasi <adam@hbang.com.au>
2018-04-18 09:42:36 +0930

    [_common/darwin] Use llvm-dsymutil if available.
    This adds support for dSYMs on all of the unofficial toolchains, as they include
    llvm-dsymutil. At least Xcode 9 already has dsymutil symlinked to llvm-dsymutil.

--- makefiles/targets/_common/darwin_head.mk
+++ makefiles/targets/_common/darwin_head.mk
@@ -57,7 +57,9 @@ TARGET_CODESIGN_ALLOCATE ?= $(call __invocation,codesign_allocate)
 TARGET_STRIP_FLAGS ?= -x
 
 ifeq ($(TARGET_DSYMUTIL),)
-ifeq ($(call __executable,dsymutil),$(_THEOS_TRUE))
+ifeq ($(call __executable,$(call __invocation,llvm-dsymutil)),$(_THEOS_TRUE))
+	TARGET_DSYMUTIL = $(call __invocation,llvm-dsymutil)
+else ifeq ($(call __executable,dsymutil),$(_THEOS_TRUE))
 	TARGET_DSYMUTIL = dsymutil
 endif
 endif
