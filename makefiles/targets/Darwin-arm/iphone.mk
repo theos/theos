29d5d85bfe2fcb4f12b4e2aeb8957ce41880ef59
uroboro <uroboro845@gmail.com>
2019-05-11 18:59:31 +0200

    [Darwin/iPhone] Fix typo in remote setting.

--- makefiles/targets/Darwin-arm/iphone.mk
+++ makefiles/targets/Darwin-arm/iphone.mk
@@ -7,7 +7,7 @@ _THEOS_TARGET_PLATFORM_SDK_NAME := iPhoneOS
 _THEOS_TARGET_PLATFORM_FLAG_NAME := iphoneos
 _THEOS_TARGET_PLATFORM_SWIFT_NAME := apple-ios
 
-TARGET_INSTALL_REMOTE := if ($(call __theos_bool,$(THEOS_INSTALL_USING_SSH)),$(_THEOS_TRUE),$(_THEOS_FALSE))
+TARGET_INSTALL_REMOTE := $(call __theos_bool,$(THEOS_INSTALL_USING_SSH))
 
 include $(THEOS_MAKE_PATH)/targets/_common/darwin_head.mk
 include $(THEOS_MAKE_PATH)/targets/_common/iphone.mk
