3a8a127c31013290b654bcc5c62fa59c8def4d51
uroboro <uroboro.845@gmail.com>
2019-06-02 21:34:45 +0200

    [package] Removed ipa installation commands

--- makefiles/install/ipa_local.mk
+++
@@ -1,5 +0,0 @@
-internal-install:: internal-install-check
-	$(ECHO_INSTALLING)appinst "$(_THEOS_PACKAGE_LAST_FILENAME)" &> /dev/null$(ECHO_END)
-
-internal-uninstall::
-	$(ECHO_NOTHING)echo "$(THEOS_PACKAGE_NAME)"$(ECHO_END)
