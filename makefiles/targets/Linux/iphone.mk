fd640b7fdaee126e34bfecb271f2f5c9514ce968
DGh0st <deeppwnage@yahoo.com>
2020-04-17 17:16:54 -0500

    Fix backwards compatibility with toolchains that do have the prefixes

--- makefiles/targets/Linux/iphone.mk
+++ makefiles/targets/Linux/iphone.mk
@@ -9,13 +9,25 @@ _THEOS_TARGET_PLATFORM_SWIFT_NAME := apple-ios
 
 SWIFTBINPATH ?= $(THEOS)/toolchain/swift/bin
 SDKBINPATH ?= $(THEOS)/toolchain/$(THEOS_PLATFORM_NAME)/$(THEOS_TARGET_NAME)/bin
-PREFIX := $(SDKBINPATH)/
+
+# Determine toolchain to use based on file existence.
+ifeq ($(SDKTARGET),)
+ifeq ($(call __exists, $(SDKBINPATH)/armv7-apple-darwin11-ld),$(_THEOS_TRUE))
+SDKTARGETPREFIX ?= armv7-apple-darwin11-
+else ifeq ($(call __exists, $(SDKBINPATH)/arm64-apple-darwin14-ld),$(_THEOS_TRUE))
+SDKTARGETPREFIX ?= arm64-apple-darwin14-
+else
+SDKTARGETPREFIX ?=
+TARGET_OPTIONS ?= -target arm64-apple-darwin
+endif
+endif
+
+PREFIX := $(SDKBINPATH)/$(SDKTARGETPREFIX)
 
 include $(THEOS_MAKE_PATH)/targets/_common/darwin_head.mk
 include $(THEOS_MAKE_PATH)/targets/_common/iphone.mk
 include $(THEOS_MAKE_PATH)/targets/_common/darwin_tail.mk
 
-TARGET_OPTIONS ?= -target arm64-apple-darwin
 _THEOS_TARGET_CFLAGS += $(TARGET_OPTIONS)
 _THEOS_TARGET_CCFLAGS += $(TARGET_OPTIONS)
 _THEOS_TARGET_LDFLAGS += $(TARGET_OPTIONS)
