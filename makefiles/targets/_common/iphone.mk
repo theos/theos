ca7e97d6064e0cb4577a39dc68e4df9355fe9440
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-06-28 14:51:29 +0100

    Change defaults to allow building arm64e on Linux

--- makefiles/targets/_common/iphone.mk
+++ makefiles/targets/_common/iphone.mk
@@ -52,7 +52,7 @@ else # } else {
 	ARCHS ?= arm64
 endif # }
 else ifeq ($(_TARGET_VERSION_GE_7_0),1) # } >= 7.0 {
-ifeq ($(_TARGET_VERSION_GE_12_1)$(THEOS_PLATFORM_NAME),1macosx) # >= 12.1, macOS {
+ifeq ($(_TARGET_VERSION_GE_12_1),1) # >= 12.1 {
 	ARCHS ?= armv7 arm64 arm64e
 else # } else {
 	ARCHS ?= armv7 arm64
