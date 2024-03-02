7c6621efb4d78378eef6ddd55d4b285072c309ee
Lightmann <lightmanns11@gmail.com>
2024-03-02 15:04:48 -0500

    Rootless v2 flags (#790)

--- vendor/mod/rootless/instance/rules.mk
+++ vendor/mod/rootless/instance/rules.mk
@@ -1,1 +1,2 @@
-_THEOS_INTERNAL_LDFLAGS += -rpath $(THEOS_PACKAGE_INSTALL_PREFIX)/Library/Frameworks -rpath $(THEOS_PACKAGE_INSTALL_PREFIX)/usr/lib
+_THEOS_INTERNAL_LDFLAGS += -rpath $(THEOS_PACKAGE_INSTALL_PREFIX)/Library/Frameworks -rpath $(THEOS_PACKAGE_INSTALL_PREFIX)/usr/lib # v1
+_THEOS_INTERNAL_LDFLAGS += -rpath '@loader_path/.jbroot/Library/Frameworks' -rpath '@loader_path/.jbroot/usr/lib' # v2
